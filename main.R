c65000e4b7c21d81a9761a20c2efe43926ca7640
ginberg <info@gerinberg.com>
2021-05-07 09:29:41 +0000

    update operator

--- main.R
+++ main.R
@@ -1,9 +1,10 @@
 library(tercen)
 library(dplyr)
 
-(ctx = tercenCtx())  %>% 
-  select(.y, .ci, .ri) %>% 
-  group_by(.ci, .ri) %>%
-  summarise(median = median(.y)) %>%
+
+(ctx = tercenCtx()) %>% 
+  select(.y, .ri, .ci) %>% 
+  group_by(.ri, .ci) %>%
+  mutate(value = 100*.y) %>%
   ctx$addNamespace() %>%
   ctx$save()
--- workspace.R
+++ workspace.R
@@ -1,15 +1,12 @@
 library(tercen)
 library(dplyr)
 
-options("tercen.workflowId" = "wwww")
-options("tercen.stepId"     = "dddd")
+options("tercen.workflowId" = "119e20e1121a1ac50b16db38b0000a3d")
+options("tercen.stepId"     = "23aa280b-9816-4425-a19c-0df1586ab443")
 
-getOption("tercen.workflowId")
-getOption("tercen.stepId")
-
-(ctx = tercenCtx())  %>% 
-  select(.y, .ci, .ri) %>% 
-  group_by(.ci, .ri) %>%
-  summarise(median = median(.y)) %>%
+(ctx = tercenCtx()) %>% 
+  select(.y, .ri, .ci) %>% 
+  group_by(.ri, .ci) %>%
+  mutate(value = 100*.y) %>%
   ctx$addNamespace() %>%
   ctx$save()
