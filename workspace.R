7cd7e9b3373401daab2ce43525f8e8d718fe0ab4
ginberg <info@gerinberg.com>
2021-05-07 09:34:53 +0000

    add renv

--- workspace.R
+++ workspace.R
@@ -2,7 +2,7 @@ library(tercen)
 library(dplyr)
 
 options("tercen.workflowId" = "119e20e1121a1ac50b16db38b0000a3d")
-options("tercen.stepId"     = "23aa280b-9816-4425-a19c-0df1586ab443")
+options("tercen.stepId"     = "b226ed77-56d7-494f-94bd-d042d36de6ea")
 
 (ctx = tercenCtx()) %>% 
   select(.y, .ri, .ci) %>% 
