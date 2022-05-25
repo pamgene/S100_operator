077da7fe31a036ca650cbf3eb7b22de73952b56d
Alexandre Maurel <alexandre.maurel@gmail.com>
2022-05-25 08:59:00 +0000

    remove group_by

--- main.R
+++ main.R
@@ -2,12 +2,11 @@ library(tercen)
 library(dplyr)
 
 ctx <- tercenCtx()
-
-if(inherits(try(ctx$select(".y")), 'try-error')) stop("y axis is missing.")
+ 
+if(length(ctx$yAxis) == 0) stop("y axis is missing.")
 
 ctx %>%
   select(.y, .ri, .ci) %>%
-  group_by(.ri, .ci) %>%
   mutate(S100 = 100*.y) %>%
   select(-.y) %>%
   ctx$addNamespace() %>%
